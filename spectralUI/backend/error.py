1c6e39603058db2a8eb69d87cb559f76d7cdfca2
Tom George Ampiath <74437170+TomAmpiath@users.noreply.github.com>
2021-04-05 18:34:26 +0530

    error handler and metadata backend logic

---
+++ spectralUI/backend/error.py
@@ -0,0 +1,63 @@
+# This file is part of spectralUI.
+#
+# Copyright 2021, Tom George Ampiath, All rights reserved.
+#
+# spectralUI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# spectralUI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with spectralUI.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+# Error Levels
+CRITICAL, WARNING, INFO = range(3)
+
+ERROR_RANGE = 4
+
+# Error IDs
+(
+    FILE_NOT_FOUND,
+    NO_SPECTRAL_DATA,
+    UNKNOWN_FORMAT,
+    ERROR_ERROR,
+) = range(ERROR_RANGE)
+
+# dict -> error id: (error message, error level)
+error_dict = {
+    FILE_NOT_FOUND: ("File Not Found", CRITICAL),
+    NO_SPECTRAL_DATA: ("No Spectral Data Found in Input File", WARNING),
+    UNKNOWN_FORMAT: ("Unknown File Format.", WARNING),
+    ERROR_ERROR: ("Unknown Error ID", CRITICAL),
+}
+
+
+def get_error_message(error_id):
+    """Get error message corresponding to error_id
+
+    :param error_id: error id
+
+    :return: message corresponding to error_id
+    """
+    if error_id not in range(ERROR_RANGE):
+        sys.exit(ERROR_ERROR)
+    return error_dict[error_id][0]
+
+
+def get_error_level(error_id):
+    """Get error level corresponding to error_id
+
+    :param error_id: error id
+
+    :return: error level corresponding to error_id
+    """
+    if error_id not in range(ERROR_RANGE):
+        sys.exit(ERROR_ERROR)
+    return error_dict[error_id][1]
---
+++ spectralUI/backend/metadata.py
@@ -0,0 +1,52 @@
+# This file is part of spectralUI.
+#
+# Copyright 2021, Tom George Ampiath, All rights reserved.
+#
+# spectralUI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# spectralUI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with spectralUI.  If not, see <http://www.gnu.org/licenses/>.
+
+from numpy import amax, amin, mean
+
+properties_list = [
+    "Height",
+    "Width",
+    "No: of Bands",
+    "Max. Intensity of Current Band",
+    "Min. Intensity of Current Band",
+    "Mean Intensity of Current Band",
+]
+
+
+def get_properties_list():
+    """Returns the list of properties"""
+    return properties_list
+
+
+def get_metadata(datacube, band_number=0):
+    """Returns a dictionary containing metadata or error code if any error occurs.
+
+    :param datacube: spectral datacube as a numpy ndarray object.
+    :param band_number: current band number (default 0)
+
+    :return: metadata dictionary
+    """
+    metadata = [
+        str(datacube.shape[0]),
+        str(datacube.shape[1]),
+        str(datacube.shape[2]),
+        str(amax(datacube[:, :, band_number])),
+        str(amin(datacube[:, :, band_number])),
+        str(mean(datacube[:, :, band_number])),
+    ]
+
+    return metadata
